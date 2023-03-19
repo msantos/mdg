f8c99af44ba74618dda5e81b6d53eca1e2cea625
Michael Santos <michael.santos@gmail.com>
2023-03-19 16:19:46 -0400

    feat: use subcommands

---
+++ cmd/mdg0/convert/run.go
@@ -0,0 +1,113 @@
+package convert
+
+import (
+	_ "embed"
+	"flag"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"text/template"
+
+	"git.iscode.ca/msantos/mdg0/markdown"
+)
+
+type State struct {
+	verbose bool
+	css     string
+
+	md *markdown.Opt
+	t  *template.Template
+}
+
+func Run() {
+	css := flag.String("css", "", "CSS")
+	tmpl := flag.String("template", "", "HTML template")
+	verbose := flag.Bool("verbose", false, "Enable debug messages")
+	flag.Parse()
+
+	dir := "."
+	if flag.NArg() > 0 {
+		dir = flag.Arg(0)
+	}
+
+	var t *template.Template
+	var err error
+
+	if *tmpl != "" {
+		t, err = template.New("index").Parse(*tmpl)
+		if err != nil {
+			log.Fatalf("template: %v\n", err)
+		}
+	}
+
+	s := &State{
+		md:      markdown.New(markdown.WithTemplate(t), markdown.WithCSS(*css)),
+		verbose: *verbose,
+	}
+
+	if err := s.run(dir); err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func (s *State) run(dir string) error {
+	return filepath.WalkDir(dir, s.convert)
+}
+
+func (s *State) newer(md, html string) bool {
+	stmd, err := os.Stat(md)
+	if err != nil {
+		return false
+	}
+
+	sthtml, err := os.Stat(html)
+	if err != nil {
+		return true
+	}
+
+	return stmd.ModTime().After(sthtml.ModTime())
+}
+
+func (s *State) convert(file string, d fs.DirEntry, err error) error {
+	if err != nil {
+		return err
+	}
+	if d.Type() != 0 {
+		return nil
+	}
+	if strings.HasPrefix(file, ".") || strings.HasPrefix(file, "_") {
+		return nil
+	}
+
+	if filepath.Ext(file) != ".md" {
+		return nil
+	}
+
+	html := strings.TrimSuffix(file, filepath.Ext(file)) + ".html"
+
+	if !s.newer(file, html) {
+		return nil
+	}
+
+	log.Println("Converting:", file, " -> ", html)
+
+	p, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	w, err := os.OpenFile(html, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if err := w.Close(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	return s.md.Convert(p, w)
+}
--- cmd/mdg0/main.go
+++ cmd/mdg0/main.go
@@ -1,113 +1,70 @@
 package main
 
 import (
-	_ "embed"
 	"flag"
-	"io/fs"
-	"log"
+	"fmt"
 	"os"
-	"path/filepath"
-	"strings"
-	"text/template"
+	"path"
 
-	"git.iscode.ca/msantos/mdg0/markdown"
+	"git.iscode.ca/msantos/mdg0/cmd/mdg0/convert"
 )
 
-type State struct {
-	verbose bool
-	css     string
+const (
+	version = "0.1.0"
+)
 
-	md *markdown.Opt
-	t  *template.Template
+var f = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `%s v%s
+Usage: %s <command> [<option>]
+
+Convert and format markdown.
+
+Commands:
+
+      convert  - convert markdown to HTML
+      fmt      - format markdown
+      version  - display version
+
+`, path.Base(os.Args[0]), version, os.Args[0])
+	fmt.Fprintf(os.Stderr, "Options:\n\n")
+	f.PrintDefaults()
 }
 
 func main() {
-	css := flag.String("css", "", "CSS")
-	tmpl := flag.String("template", "", "HTML template")
-	verbose := flag.Bool("verbose", false, "Enable debug messages")
-	flag.Parse()
+	f.Usage = func() { usage() }
+	_ = f.Parse(os.Args[1:])
 
-	dir := "."
-	if flag.NArg() > 0 {
-		dir = flag.Arg(0)
+	oargs := f.Args()
+
+	command := "help"
+
+	if len(os.Args) > 1 {
+		command = oargs[0]
 	}
 
-	var t *template.Template
-	var err error
-
-	if *tmpl != "" {
-		t, err = template.New("index").Parse(*tmpl)
-		if err != nil {
-			log.Fatalf("template: %v\n", err)
-		}
+	var args []string
+	if len(oargs) > 1 {
+		args = oargs[1:]
 	}
 
-	s := &State{
-		md:      markdown.New(markdown.WithTemplate(t), markdown.WithCSS(*css)),
-		verbose: *verbose,
+	os.Args = append(os.Args[:1], args...)
+
+	switch command {
+	case "convert":
+		convert.Run()
+	case "format":
+		fallthrough
+	case "fmt":
+	case "help":
+		usage()
+	case "version":
+		fmt.Println(version)
+	default:
+		fmt.Println("command not found:", command)
+		os.Exit(127)
 	}
 
-	if err := s.run(dir); err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func (s *State) run(dir string) error {
-	return filepath.WalkDir(dir, s.convert)
-}
-
-func (s *State) newer(md, html string) bool {
-	stmd, err := os.Stat(md)
-	if err != nil {
-		return false
-	}
-
-	sthtml, err := os.Stat(html)
-	if err != nil {
-		return true
-	}
-
-	return stmd.ModTime().After(sthtml.ModTime())
-}
-
-func (s *State) convert(file string, d fs.DirEntry, err error) error {
-	if err != nil {
-		return err
-	}
-	if d.Type() != 0 {
-		return nil
-	}
-	if strings.HasPrefix(file, ".") || strings.HasPrefix(file, "_") {
-		return nil
-	}
-
-	if filepath.Ext(file) != ".md" {
-		return nil
-	}
-
-	html := strings.TrimSuffix(file, filepath.Ext(file)) + ".html"
-
-	if !s.newer(file, html) {
-		return nil
-	}
-
-	log.Println("Converting:", file, " -> ", html)
-
-	p, err := os.ReadFile(file)
-	if err != nil {
-		return err
-	}
-
-	w, err := os.OpenFile(html, os.O_RDWR|os.O_CREATE, 0644)
-	if err != nil {
-		return err
-	}
-
-	defer func() {
-		if err := w.Close(); err != nil {
-			log.Println(err)
-		}
-	}()
-
-	return s.md.Convert(p, w)
+	os.Exit(0)
 }
